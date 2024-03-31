75eb67017d808f5df64da652d5fce7b0a4427134
Yiqun Chen <yq_90@protonmail.com>
2024-03-31 20:02:25 +0800

    Adding goal_pose from different frame
    Commander now compute the ground_zero point by the frame from which the
    goal_pose was set.

--- px4_commander/include/px4_commander/commander.hpp
+++ px4_commander/include/px4_commander/commander.hpp
@@ -8,6 +8,10 @@
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "tf2_ros/transform_broadcaster.h"
 
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+
 #include "frame_transforms/frame_transforms.hpp"
 
 #include "Eigen/Eigen"
@@ -23,7 +27,7 @@ class Commander
     public:
         using PoseStampedTy = geometry_msgs::msg::PoseStamped;
         using OdomTy = nav_msgs::msg::Odometry;
-        using GoalPoseStampedTy = geometry_msgs::msg::PoseStamped;
+        using GoalPoseStampedTy = PoseStampedTy;
 
         Commander(rclcpp::Node::SharedPtr node) :
             commander_node(node), frame_(node->get_parameter("frame").as_string()) {
@@ -47,6 +51,9 @@ class Commander
 
                 command_frame_broadcaster_->sendTransform(ground_zero_transform_);
 
+                goal_pose_buffer_ = std::make_unique<tf2_ros::Buffer>(commander_node->get_clock());
+                goal_listener_ = std::make_shared<tf2_ros::TransformListener>(*goal_pose_buffer_);
+
                 auto broadcasting_tf = [this]() -> void {
                     this->ground_zero_transform_.header.stamp =
                         this->commander_node->get_clock()->now();
@@ -58,14 +65,29 @@ class Commander
                     this->ground_zero_transform_.header.stamp =
                         this->commander_node->get_clock()->now();
 
-                    this->ground_zero_transform_.transform.translation.x = msg.pose.position.x;
-                    this->ground_zero_transform_.transform.translation.y = msg.pose.position.y;
-                    this->ground_zero_transform_.transform.translation.z = msg.pose.position.z;
+                    PoseStampedTy map_goal_pose;
+                    try {
+                        // Don't know why, but is seems we need a timeout for transform
+                        // Otherwise this would yield error in message timestamp while looking
+                        // for the corresponding frame
+                        this->goal_pose_buffer_->transform(msg, map_goal_pose, "map",
+                                tf2::durationFromSec(10));
+                    } catch (const tf2::TransformException &e) {
+                        RCLCPP_ERROR(this->commander_node->get_logger(),
+                                "Failed to transform %s -> %s",
+                                msg.header.frame_id.c_str(),
+                                "map");
+                        return ;
+                    }
 
-                    this->ground_zero_transform_.transform.rotation.x = msg.pose.orientation.x;
-                    this->ground_zero_transform_.transform.rotation.y = msg.pose.orientation.y;
-                    this->ground_zero_transform_.transform.rotation.z = msg.pose.orientation.z;
-                    this->ground_zero_transform_.transform.rotation.w = msg.pose.orientation.w;
+                    this->ground_zero_transform_.transform.translation.x = map_goal_pose.pose.position.x;
+                    this->ground_zero_transform_.transform.translation.y = map_goal_pose.pose.position.y;
+                    this->ground_zero_transform_.transform.translation.z = map_goal_pose.pose.position.z;
+
+                    this->ground_zero_transform_.transform.rotation.x = map_goal_pose.pose.orientation.x;
+                    this->ground_zero_transform_.transform.rotation.y = map_goal_pose.pose.orientation.y;
+                    this->ground_zero_transform_.transform.rotation.z = map_goal_pose.pose.orientation.z;
+                    this->ground_zero_transform_.transform.rotation.w = map_goal_pose.pose.orientation.w;
 
                     command_frame_broadcaster_->sendTransform(ground_zero_transform_);
                 };
@@ -83,6 +105,9 @@ class Commander
 
         geometry_msgs::msg::TransformStamped ground_zero_transform_;
         rclcpp::TimerBase::SharedPtr tf_timer_;
+
+        std::unique_ptr<tf2_ros::Buffer> goal_pose_buffer_;
+        std::shared_ptr<tf2_ros::TransformListener> goal_listener_;
 };
 
 #endif
